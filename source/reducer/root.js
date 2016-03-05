c22924c6589b72a3bf4078b42c6e99af31725b88
Lucas Correia <lucas.correia@ftrack.com>
2016-03-05 23:12:44 +0100

    Fix root reducer style

--- source/reducer/root.js
+++ source/reducer/root.js
@@ -1,10 +1,8 @@
 // :copyright: Copyright (c) 2016 ftrack
-/*eslint-disable */
 
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import userReducer from './user';
-
 
 export default combineReducers({
     routing: routerReducer,
