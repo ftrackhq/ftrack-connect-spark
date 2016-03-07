dad2b34f904802648de853e72b2cec0d202e223c
Lucas Correia <lucas.correia@ftrack.com>
2016-03-07 11:39:15 +0100

    Fix linter issue, convert root container to stateless

--- source/container/root.js
+++ source/container/root.js
@@ -6,16 +6,14 @@ import { Router } from 'react-router';
 
 
 /** A root container. */
-class RootContainer extends React.Component {
-    render() {
-        return (
-            <Provider store={this.props.store}>
-                <Router history={this.props.history}>
-                    {this.props.routes}
-                </Router>
-            </Provider>
-        );
-    }
+function RootContainer() {
+    return (
+        <Provider store={this.props.store}>
+            <Router history={this.props.history}>
+                {this.props.routes}
+            </Router>
+        </Provider>
+    );
 }
 
 RootContainer.propTypes = {
