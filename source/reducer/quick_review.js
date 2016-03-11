60c843379764207d219e2a65d1ede301778627e1
Lucas Correia <lucas.correia@ftrack.com>
2016-03-11 15:45:04 +0100

    Fix misc docstrings

--- source/reducer/quick_review.js
+++ source/reducer/quick_review.js
@@ -1,5 +1,8 @@
+// :copyright: Copyright (c) 2016 ftrack
+
 import types from 'action/quick_review';
 
+/** Reduce actions to quickReview state. */
 export default function quickReviewReducer(state = null, action) {
     let nextState = state;
     if (action.type === types.QUICK_REVIEW_PROJECTS_LOADED) {
