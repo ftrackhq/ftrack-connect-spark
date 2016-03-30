df14b420833be47bc97cb4bbe67139ae7125e9e7
Björn Rydahl <bjorn.rydahl@ftrack.com>
2016-03-30 11:23:44 +0200

    Update context view.

--- source/container/context_browser/view.js
+++ source/container/context_browser/view.js
@@ -3,8 +3,12 @@
 import React from 'react';
 
 import InfiniteScroll from 'component/infinite_scroll';
-import { ListItem, List } from 'react-toolbox';
+import { List } from 'react-toolbox';
+import { Card, CardActions, CardTitle, CardMedia, CardText } from 'react-toolbox/lib/card';
+import Button from 'react-toolbox/lib/button';
+import Reveal from 'component/reveal';
 
+import style from './style';
 import { session } from '../../ftrack_api';
 
 
@@ -31,12 +35,12 @@ class ContextBrowser extends React.Component {
 
         if (parentId !== 'projects') {
             this._baseQuery = (
-                'select id, name, thumbnail_id from TypedContext where ' +
+                'select id, name, thumbnail_id, link from TypedContext where ' +
                 `parent_id is ${parentId} order by name, id`
             );
         } else {
             this._baseQuery = (
-                'select id, name, thumbnail_id from Project order by name, id'
+                'select id, name, thumbnail_id, link from Project order by name, id'
             );
         }
     }
@@ -75,20 +79,47 @@ class ContextBrowser extends React.Component {
         }
     }
 
+    _getLink(link) {
+        const path = [];
+
+        for (let index = 0; index < link.length; index++) {
+            path.push(link[index].name);
+        }
+
+        return path.join(' / ');
+    }
+
     /** Render item. */
     _renderItem(item) {
         const onClick = this._onClick.bind(this, item);
 
         return (
-            <ListItem
+            <Card
                 key={ item.id }
-                avatar={ session.thumbnail(item.thumbnail_id, 100) }
-                caption={ item.name }
-                legend={ item.__entity_type__ }
-                rightIcon="star"
-                onClick={ onClick }
-                selectable={ 1 }
-            />
+            >
+                <div className={style.cardRow}>
+                    <CardTitle>
+                        <p>{ item.__entity_type__ }</p>
+                        <p>{ item.name }</p>
+                        <p>{ this._getLink(item.link.slice(-2, -1)) }</p>
+                        <p>{ this._getLink(item.link.slice(0, -2)) }</p>
+                    </CardTitle>
+                    <CardMedia
+                        className={ style.media }
+                        image={ session.thumbnail(item.thumbnail_id, 300) }
+                    />
+                </div>
+
+
+                <CardActions>
+                    <Button
+                        label="Details"
+                        flat
+                        onClick={ onClick }
+                        type="button"
+                    />
+                </CardActions>
+            </Card>
         );
     }
 
