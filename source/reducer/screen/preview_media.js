b46de1f5ddd62638ee82fe55646abb974682e2c0
Mattias Lagergren <mattias.lagergren@ftrack.com>
2016-04-08 11:04:59 +0200

    Remove index change from state

--- source/reducer/screen/preview_media.js
+++ source/reducer/screen/preview_media.js
@@ -7,7 +7,6 @@ export default function previewMediaReducer(state = {}, action) {
     let nextState = state;
 
     if (action.type === types.OPEN_PRVIEW_MEDIA) {
-
         nextState = Object.assign(
             {}, state,
             {
@@ -16,15 +15,8 @@ export default function previewMediaReducer(state = {}, action) {
                 components: action.payload.components,
             }
         );
-
     } else if (action.type === types.HIDE_PREVIEW_MEDIA) {
-
         nextState = Object.assign({}, state, {visible: false});
-
-    } else if (action.type === types.CHANGE_ACTIVE_PREVIEW_MEDIA) {
-
-        nextState = Object.assign({}, state, {index: action.payload.index});
-
     }
 
     return nextState;
