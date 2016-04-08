a16d14b191fe25488378a880d59508b961553d74
Lucas Correia <lucas.correia@ftrack.com>
2016-04-08 10:40:24 +0200

    Add fonts as static media, copy to dist

--- config/dev.js
+++ config/dev.js
@@ -4,6 +4,7 @@ const path = require('path');
 const webpack = require('webpack');
 const baseConfig = require('./base');
 const defaultSettings = require('./defaults');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = Object.assign({}, baseConfig, {
     entry: {
@@ -17,6 +18,22 @@ const config = Object.assign({}, baseConfig, {
     cache: true,
     devtool: 'eval-source-map',
     plugins: [
+        new CopyWebpackPlugin([
+            { from: path.join(__dirname, '../source/favicon.ico'), to: '../' },
+            {
+                from: path.join(__dirname, '../source/index.html'),
+                to: '../main/index.html',
+            },
+            {
+                from: path.join(__dirname, '../source/application/adobe/index.html'),
+                to: '../adobe/index.html',
+            },
+            {
+                from: path.join(__dirname, '../source/application/cinema4d/index.html'),
+                to: '../cinema4d/index.html',
+            },
+            { from: path.join(__dirname, '../source/static') },
+        ]),
         new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
         new webpack.ProvidePlugin({
             fetch: 'exports?self.fetch!whatwg-fetch',
