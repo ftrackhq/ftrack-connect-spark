73c03893ab9bf268f580f8e6e7ef30b69d3d70bd
Lucas Correia <lucas.correia@ftrack.com>
2016-04-08 10:36:32 +0200

    Add abstract methods to mediator

--- source/application/abstract_mediator.js
+++ source/application/abstract_mediator.js
@@ -4,16 +4,34 @@
  * Abstract Mediator
  */
 export default class AbstractMediator {
+    /** Return ftrack API credentials. */
+    getCredentials() {
+        throw new Error('Not implemented');
+    }
 
     getPublishOptions() {
         throw new Error('Not implemented');
     }
 
-    exportMedia(options) { // eslint-disable-line no-unused-vars
+    // eslint-disable-next-line no-unused-vars
+    exportMedia(options) {
         throw new Error('Not implemented');
     }
 
-    uploadMedia(options) { // eslint-disable-line no-unused-vars
+    // eslint-disable-next-line no-unused-vars
+    uploadMedia(options) {
+        throw new Error('Not implemented');
+    }
+
+    /** Return components to import for *options*. */
+    // eslint-disable-next-line no-unused-vars
+    getImportComponents(options) {
+        throw new Error('Not implemented');
+    }
+
+    /** Import *component* and resolve on success. */
+    // eslint-disable-next-line no-unused-vars
+    importComponent(component) {
         throw new Error('Not implemented');
     }
 }
