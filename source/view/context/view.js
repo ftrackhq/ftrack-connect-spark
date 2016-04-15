ababfdbbc480c160bf624b0f95ad77a72b0bd7b0
Lucas Correia <lucas.correia@ftrack.com>
2016-04-15 16:25:17 +0200

    Fix versions page is smushed when viewing lots of versions

--- source/view/context/view.js
+++ source/view/context/view.js
@@ -122,17 +122,19 @@ class _ContextView extends React.Component {
         ];
         return (
             <div className={style.view}>
-                <HomeHeader back context={contextId} />
-                <ContextBar
-                    entity={entity}
-                    onEntityUpdate={this._onEntityUpdate}
-                />
-                <ContextCard entity={entity} flat className={style.info} />
-                <div className={style.tabs}>
-                    <RouteTabs
-                        items={tabs}
-                        baseRoute={`/context/${contextType}/${contextId}/`}
+                <div className={style['view-top']}>
+                    <HomeHeader back context={contextId} />
+                    <ContextBar
+                        entity={entity}
+                        onEntityUpdate={this._onEntityUpdate}
                     />
+                    <ContextCard entity={entity} flat className={style.info} />
+                    <div className={style.tabs}>
+                        <RouteTabs
+                            items={tabs}
+                            baseRoute={`/context/${contextType}/${contextId}/`}
+                        />
+                    </div>
                 </div>
                 {this.props.children}
             </div>
